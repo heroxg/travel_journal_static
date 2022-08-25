229495d1fdddce4f079485b568bc4a09bc6b7c59
heroxg <henryuyigue@gmail.com>
2022-08-25 22:47:42 +0100

    create an array of data for the journal app

---
+++ src/data.js
@@ -0,0 +1,29 @@
+const Data = [
+    {
+        "id": 1,
+        "country": "Japan",
+        "map_url": "https://source.unsplash.com/WLxQvbMyfas",
+        "title": "Mount Fuji",
+        "start_date": "12 Jan, 2021",
+        "end_date": "24 Jan, 2021",
+        "description": "Mount Fuji is the tallest mountain in Japan, Standing at 3.776 Meters (12, 380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+    },
+    {
+        "id": "2",
+        "country": "Austrialia",
+        "map_url":"https://source.unsplash.com/JmuyB_LibRo",
+        "title": "Sydney Opera House",
+        "start_date": "27 May, 2021",
+        "end_date": "8 jun, 2021",
+        "description": "The Sydney Opera House is a multi-venue performing arts centre in Syndney. Located on the banks of the sydney Harbour, It is often regarded as one of the 20th century's most famous and distinctive buldings"
+    },
+    {
+        "id": "3",
+        "country": "Norway",
+        "map_url": "https://source.unsplash.com/3PeSjpLVtLg",
+        "title": "Geirangerfjord",
+        "start_date": "01 oct, 2021",
+        "end_date": "18 Nov, 2021",
+        "description": "The Geiranger Fjord is a fjord in the summore region of more og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
+    }
+]
